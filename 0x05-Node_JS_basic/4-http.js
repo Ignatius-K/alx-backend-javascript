00df16c4fc834254d72a860a48339ad83d75409d
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-12-07 02:00:27 +0300

    4. Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -9,7 +9,7 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/plain' });
-  res.end('Hello ALX!');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
