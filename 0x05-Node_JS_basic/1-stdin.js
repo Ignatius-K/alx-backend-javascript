11b8894968b0d2dd1363a60ff8194761899fc123
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-12-07 00:11:48 +0300

    1. Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+/**
+ * 1-stdin.js
+ *
+ * Work with std
+ */
+
+process.stdout.write('Welcome to ALX, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
