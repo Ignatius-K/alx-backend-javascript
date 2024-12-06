e77a0601277df5ec5f09593097912d2b3b4480b5
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-12-07 00:14:18 +0300

    1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@
  * Work with std
  */
 
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.toString()}`);
