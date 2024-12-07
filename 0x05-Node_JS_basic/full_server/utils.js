b5c1ccd85338c4ef7560ae40ed6f926f4032bafa
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-12-07 05:05:25 +0300

    8. Organize a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -28,7 +28,6 @@ export default function formatData(_data) {
 export function logData(data) {
   let message = '';
   const students = Object.values(data).reduce((prev, curr) => prev + curr.length, 0);
-  message += `Number of students: ${students}\n`;
   for (const field of Object.keys(data)) {
     const _students = data[field];
     message += `Number of students in ${
