8468208dbe31e3b317534da0cdc978c8f12a24b4
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-12-08 12:10:10 +0300

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -24,33 +24,31 @@ describe('0-calcul tests', function() {
       assertArgs(args)
     })
 
-    describe('Args are rounded', function() {
-      it('Rounds first arg', function() {
-        const args = {
-          '[1.3, 2]': 3,
-          '[1.5, 2]': 4,
-          '[1, 2]': 3
-        }
-        assertArgs(args)
-      })
+    it('Rounds first arg', function() {
+      const args = {
+        '[1.3, 2]': 3,
+        '[1.5, 2]': 4,
+        '[1, 2]': 3
+      }
+      assertArgs(args)
+    })
 
-      it('Rounds second arg', function() {
-        const args = {
-          '[1, 2.3]': 3,
-          '[1, 2.7]': 4,
-          '[1, 2]': 3
-        }
-        assertArgs(args)
-      })
+    it('Rounds second arg', function() {
+      const args = {
+        '[1, 2.3]': 3,
+        '[1, 2.7]': 4,
+        '[1, 2]': 3
+      }
+      assertArgs(args)
+    })
 
-      it('Rounds all args', function() {
-        const args = {
-          '[1.3, 2.3]': 3,
-          '[1.5, 2.7]': 5,
-          '[1, 2]': 3
-        }
-        assertArgs(args)
-      })
+    it('Rounds all args', function() {
+      const args = {
+        '[1.3, 2.3]': 3,
+        '[1.5, 2.7]': 5,
+        '[1, 2]': 3
+      }
+      assertArgs(args)
     })
   })
 })
