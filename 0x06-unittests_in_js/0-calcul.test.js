64c974f09cd97dc85dd0da1a3cd4801d63a0aca5
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-12-08 12:14:13 +0300

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,49 +7,46 @@ const assert = require('assert')
 
 const calculateNumber = require('./0-calcul')
 
-describe('0-calcul tests', function() {
-  describe('calculateNumber function tests', function () {
-    function assertArgs (args) {
-      Object.keys(args).forEach(key => {
-        assert.equal(calculateNumber(...JSON.parse(key)), args[key])
-      })
+describe('calculateNumber', function () {
+  function assertArgs (args) {
+    Object.keys(args).forEach(key => {
+      assert.equal(calculateNumber(...JSON.parse(key)), args[key])
+    })
+  }
+
+  it('Expects two args', function () {
+    const args = {
+      '[]': NaN,
+      '[1]': NaN,
+      '[1, 1]': 2 
     }
+    assertArgs(args)
+  })
 
-    it('Expects two args', function () {
-      const args = {
-        '[]': NaN,
-        '[1]': NaN,
-        '[1, 1]': 2 
-      }
-      assertArgs(args)
-    })
+  it('Rounds first arg', function() {
+    const args = {
+      '[1.3, 2]': 3,
+      '[1.5, 2]': 4,
+      '[1, 2]': 3
+    }
+    assertArgs(args)
+  })
 
-    it('Rounds first arg', function() {
-      const args = {
-        '[1.3, 2]': 3,
-        '[1.5, 2]': 4,
-        '[1, 2]': 3
-      }
-      assertArgs(args)
-    })
+  it('Rounds second arg', function() {
+    const args = {
+      '[1, 2.3]': 3,
+      '[1, 2.7]': 4,
+      '[1, 2]': 3
+    }
+    assertArgs(args)
+  })
 
-    it('Rounds second arg', function() {
-      const args = {
-        '[1, 2.3]': 3,
-        '[1, 2.7]': 4,
-        '[1, 2]': 3
-      }
-      assertArgs(args)
-    })
-
-    it('Rounds all args', function() {
-      const args = {
-        '[1.3, 2.3]': 3,
-        '[1.5, 2.7]': 5,
-        '[1, 2]': 3
-      }
-      assertArgs(args)
-    })
+  it('Rounds all args', function() {
+    const args = {
+      '[1.3, 2.3]': 3,
+      '[1.5, 2.7]': 5,
+      '[1, 2]': 3
+    }
+    assertArgs(args)
   })
 })
-
