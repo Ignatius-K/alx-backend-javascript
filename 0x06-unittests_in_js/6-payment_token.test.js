b497052e88eaf4bf11d9f8084ba28af714ed23b0
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-12-08 18:59:18 +0300

    6. Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -3,9 +3,16 @@
  *
  * Defines tests for module '6-payment_token'
  */
+const { expect } = require('chai')
+
 const getPaymentTokenFromAPI = require('./6-payment_token')
 
 describe('getPaymentTokenFromAPI', function () {
+  it('Test the function when success is false', function () {
+    const response = getPaymentTokenFromAPI(false)
+    expect(response).to.be.instanceof(Promise)
+  })
+
   it('Test the function when success is true', function (done) {
     getPaymentTokenFromAPI(true)
       .then(() => done())
